49b74785926f4428492cc4ad0272fa5d3dcebca9
wsprtv <wsprtv@users.noreply.github.com>
2025-07-23 14:28:17 -0700

    Added missing OSM tile for zoom level 0

--- wsprtv.js
+++ wsprtv.js
@@ -1517,7 +1517,8 @@ function Run() {
 
   // Add day / night visualization and the scale indicator
   let terminator = L.terminator(
-      {opacity : 0, fillOpacity : 0.3, interactive : false}).addTo(map);
+      {opacity : 0, fillOpacity : 0.3, interactive : false,
+       longitudeRange: 360}).addTo(map);
   L.control.scale().addTo(map);
 
   // Draw the antimeridian
